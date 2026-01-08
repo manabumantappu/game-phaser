8a46b4ae2a703583d352461622c9574d44d91f1d
manabumantappu <azishachigo@gmail.com>
2026-01-08 16:20:06 +0700

    Update BootScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -4,20 +4,22 @@ export default class BootScene extends Phaser.Scene {
   }
 
   preload() {
-    /* ======================
-       AUDIO
-    ====================== */
+    // AUDIO
+    this.load.audio("bgm", "assets/audio/bgm.mp3");
     this.load.audio("click", "assets/audio/click.wav");
     this.load.audio("collect", "assets/audio/collect.wav");
-    this.load.audio("bgm", "assets/audio/bgm.mp3");
 
-    /* ======================
-       SPRITE (placeholder)
-    ====================== */
-    this.load.image("player", "assets/player.png");
-    this.load.image("pellet", "assets/pellet.png");
-    this.load.image("wall", "assets/wall.png");
-    this.load.image("goal", "assets/goal.png");
+    // SPRITES
+    this.load.spritesheet("pacman", "assets/sprites/pacman.png", {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+
+    this.load.image("ghost", "assets/sprites/ghost.png");
+    this.load.image("pellet", "assets/sprites/pellet.png");
+    this.load.image("power", "assets/sprites/power.png");
+    this.load.image("wall", "assets/sprites/wall.png");
+    this.load.image("goal", "assets/sprites/goal.png");
   }
 
   create() {
