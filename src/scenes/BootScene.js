3dd98472790fa5678c0b839ea0bbcd53f00cdcbd
manabumantappu <azishachigo@gmail.com>
2026-01-08 17:47:10 +0700

    Update BootScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -9,17 +9,12 @@ export default class BootScene extends Phaser.Scene {
     this.load.audio("click", "assets/audio/click.wav");
     this.load.audio("collect", "assets/audio/collect.wav");
 
-    // SPRITES
-    this.load.spritesheet("pacman", "assets/sprites/pacman.png", {
-      frameWidth: 32,
-      frameHeight: 32
-    });
-
+    // IMAGE (SATU PACMAN)
+    this.load.image("pacman", "assets/sprites/pacman.png");
     this.load.image("ghost", "assets/sprites/ghost.png");
     this.load.image("pellet", "assets/sprites/pellet.png");
     this.load.image("power", "assets/sprites/power.png");
     this.load.image("wall", "assets/sprites/wall.png");
-    this.load.image("goal", "assets/sprites/goal.png");
   }
 
   create() {
