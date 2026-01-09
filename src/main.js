b7da9a19d27c633fe6bd09eb9d945e6eb514914d
manabumantappu <azishachigo@gmail.com>
2026-01-09 19:51:29 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,27 +2,15 @@ import BootScene from "./scenes/BootScene.js";
 import MenuScene from "./scenes/MenuScene.js";
 import GameScene from "./scenes/GameScene.js";
 
-const config = {
+new Phaser.Game({
   type: Phaser.AUTO,
-  width: 360,
+  width: 480,
   height: 640,
   backgroundColor: "#000000",
   parent: "game",
-
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
-
-  input: {
-    activePointers: 3
-  },
-
-  scene: [
-    BootScene,
-    MenuScene,
-    GameScene
-  ]
-};
-
-new Phaser.Game(config);
+  scene: [BootScene, MenuScene, GameScene]
+});
