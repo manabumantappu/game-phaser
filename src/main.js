87978cb502e9ec0f05eede6edf011d9480d2d8d0
manabumantappu <azishachigo@gmail.com>
2026-01-09 16:33:32 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,20 +5,30 @@ import GameScene from "./scenes/GameScene.js";
 const config = {
   type: Phaser.AUTO,
   width: 480,
-  height: 640,
+  height: 800,
   backgroundColor: "#000000",
-  parent: "game",
-  scene: [BootScene, MenuScene, GameScene]
-};
 
-new Phaser.Game({
-  type: Phaser.AUTO,
-  width: 480,
-  height: 640,
-  backgroundColor: "#000000",
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
-  scene: [BootScene, MenuScene, GameScene]
-});
+
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+
+  input: {
+    activePointers: 3
+  },
+
+  scene: [
+    BootScene,
+    MenuScene,
+    GameScene
+  ]
+};
+
+new Phaser.Game(config);
